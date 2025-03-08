d87cd50ab833577b432ae824215c684fa2fe6afb
Joaquin Tech Tips <joaquin.alcazar.7e4@itb.cat>
2025-03-08 23:15:02 +0100

    Hey mira, funciona
    EL nacimiento de Tareas del Hogar 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public enum States
 {
     CanMove,
     CantMove
 }
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
@@ -15,19 +17,22 @@ public class GameManager : MonoBehaviour
     public int[,] Matrix;
     [SerializeField] private States state = States.CanMove;
     public Camera camera;
+
     void Start()
     {
         Instance = this;
         Matrix = new int[Size, Size];
         Calculs.CalculateDistances(collider, Size);
+
         for (int i = 0; i < Size; i++)
         {
             for (int j = 0; j < Size; j++)
             {
-                Matrix[i, j] = 0; // 0: desocupat, 1: fitxa jugador 1, -1: fitxa IA;
+                Matrix[i, j] = 0;
             }
         }
     }
+
     private void Update()
     {
         if (state == States.CanMove)
@@ -35,34 +40,118 @@ public class GameManager : MonoBehaviour
             Vector3 m = Input.mousePosition;
             m.z = 10f;
             Vector3 mousepos = camera.ScreenToWorldPoint(m);
+
             if (Input.GetMouseButtonDown(0))
             {
                 if (Calculs.CheckIfValidClick((Vector2)mousepos, Matrix))
                 {
                     state = States.CantMove;
-                    if(Calculs.EvaluateWin(Matrix)==2)
+                    if (Calculs.EvaluateWin(Matrix) == 2)
                         StartCoroutine(WaitingABit());
                 }
             }
         }
     }
+
     private IEnumerator WaitingABit()
     {
         yield return new WaitForSeconds(1f);
-        RandomAI();
+
+        MinimaxAI();
+        //RandomAI();
     }
+
     public void RandomAI()
     {
-        int x;
-        int y;
+        int x, y;
         do
         {
             x = Random.Range(0, Size);
             y = Random.Range(0, Size);
         } while (Matrix[x, y] != 0);
+
         DoMove(x, y, -1);
         state = States.CanMove;
     }
+
+    public void MinimaxAI()
+    {
+        int bestScore = int.MinValue;
+        int moveX = -1, moveY = -1;
+
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                if (Matrix[i, j] == 0)
+                {
+                    Matrix[i, j] = -1;
+                    int score = Minimax(Matrix, 0, false);
+                    Matrix[i, j] = 0;
+
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                        moveX = i;
+                        moveY = j;
+                    }
+                }
+            }
+        }
+
+        if (moveX != -1 && moveY != -1)
+        {
+            DoMove(moveX, moveY, -1);
+            state = States.CanMove;
+        }
+    }
+
+    private int Minimax(int[,] board, int depth, bool isMaximizing)
+    {
+        int result = Calculs.EvaluateWin(board);
+        if (result == 1) return -10 + depth; // Gana el jugador
+        if (result == -1) return 10 - depth; // Gana la IA
+        if (result == 0) return 0; // Empate
+
+        if (isMaximizing)
+        {
+            int bestScore = int.MinValue;
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        board[i, j] = -1; 
+                        int score = Minimax(board, depth + 1, false);
+                        board[i, j] = 0; 
+                        bestScore = Mathf.Max(score, bestScore);
+                    }
+                }
+            }
+            return bestScore;
+        }
+        else
+        {
+            int bestScore = int.MaxValue;
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        board[i, j] = 1; 
+                        int score = Minimax(board, depth + 1, true);
+                        board[i, j] = 0; 
+                        bestScore = Mathf.Min(score, bestScore);
+                    }
+                }
+            }
+            return bestScore;
+        }
+    }
+
+
     public void DoMove(int x, int y, int team)
     {
         Matrix[x, y] = team;
@@ -70,6 +159,7 @@ public class GameManager : MonoBehaviour
             Instantiate(token1, Calculs.CalculatePoint(x, y), Quaternion.identity);
         else
             Instantiate(token2, Calculs.CalculatePoint(x, y), Quaternion.identity);
+
         int result = Calculs.EvaluateWin(Matrix);
         switch (result)
         {
@@ -83,7 +173,7 @@ public class GameManager : MonoBehaviour
                 Debug.Log("You Lose");
                 break;
             case 2:
-                if(state == States.CantMove)
+                if (state == States.CantMove)
                     state = States.CanMove;
                 break;
         }
